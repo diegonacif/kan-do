6fe52637e255339c5dc95721899b3e43a4d482cf
diegonacif <diegoreisnacif@gmail.com>
2023-03-03 21:04:00 -0300

    style: header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Sidebar } from './components/Sidebar/Sidebar';
+import { Header } from './components/Header/Header';
 import './css/App.css';
 
 export const App = () => {
   return (
     <div className="App">
-      <Sidebar />
+      {/* <Sidebar /> */}
+      <Header />
     </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,13 @@
+import { DotsThreeOutlineVertical } from "phosphor-react"
+
 export const Header = () => {
   return (
     <div className="header-container">
-      <h2>HEADER</h2>
+      <h3>Nome do quadro</h3>
+      <section>
+        <button>Adicionar nova tarefa</button>
+        <DotsThreeOutlineVertical size={20} weight="fill" />
+      </section>
     </div>
   )
 }
