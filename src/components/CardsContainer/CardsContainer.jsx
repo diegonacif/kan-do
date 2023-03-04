5287364113daca0c2d77dd8a9d54aa7176b6f6db
diegonacif <diegoreisnacif@gmail.com>
2023-03-03 21:29:15 -0300

    refactor: new kan favicon

--- src/components/CardsContainer/CardsContainer.jsx
+++ src/components/CardsContainer/CardsContainer.jsx
@@ -6,7 +6,7 @@ export const CardsContainer = () => {
       <section>
         <div className="dot"></div>
         <div className="cards-wrapper">
-
+          
         </div>
       </section>
     </div>
