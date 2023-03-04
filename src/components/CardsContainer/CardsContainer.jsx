2039fecb62299794fc359808a32b258889f07672
diegonacif <diegoreisnacif@gmail.com>
2023-03-03 21:50:09 -0300

    refactor: cards container grid and sections titles

--- src/components/CardsContainer/CardsContainer.jsx
+++ src/components/CardsContainer/CardsContainer.jsx
@@ -1,3 +1,4 @@
+import { Circle } from 'phosphor-react';
 import '../../css/App.css';
 import { KanCard } from '../KanCard/KanCard';
 
@@ -5,7 +6,28 @@ export const CardsContainer = () => {
   return (
     <div className="cards-container-container">
       <section>
-        <div className="dot"></div>
+        <div className="section-title">
+          <Circle size={13} color="#12a9ca" weight="fill" />
+          <span>TODO (4)</span>
+        </div>
+        <div className="cards-wrapper">
+          <KanCard />
+        </div>
+      </section>
+      <section>
+      <div className="section-title">
+          <div className="color-dot"></div>
+          <span>DOING (4)</span>
+        </div>
+        <div className="cards-wrapper">
+          <KanCard />
+        </div>
+      </section>
+      <section>
+      <div className="section-title">
+          <div className="color-dot"></div>
+          <span>DONE (4)</span>
+        </div>
         <div className="cards-wrapper">
           <KanCard />
         </div>
