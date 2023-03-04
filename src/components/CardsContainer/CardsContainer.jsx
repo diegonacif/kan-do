6028b7e0d4277dc1cfbaba4e4e34b1850ef621ed
diegonacif <diegoreisnacif@gmail.com>
2023-03-03 22:02:56 -0300

    refactor: cards container title color dots

--- src/components/CardsContainer/CardsContainer.jsx
+++ src/components/CardsContainer/CardsContainer.jsx
@@ -7,28 +7,34 @@ export const CardsContainer = () => {
     <div className="cards-container-container">
       <section>
         <div className="section-title">
-          <Circle size={13} color="#12a9ca" weight="fill" />
+          <Circle size={13} color="#f1e585" weight="fill" />
           <span>TODO (4)</span>
         </div>
         <div className="cards-wrapper">
           <KanCard />
-        </div>
-      </section>
-      <section>
-      <div className="section-title">
-          <div className="color-dot"></div>
-          <span>DOING (4)</span>
-        </div>
-        <div className="cards-wrapper">
+          <KanCard />
           <KanCard />
         </div>
       </section>
       <section>
-      <div className="section-title">
-          <div className="color-dot"></div>
+        <div className="section-title">
+          <Circle size={13} color="#12a9ca" weight="fill" />
+          <span>DOING (4)</span>
+        </div>
+        <div className="cards-wrapper">
+          <KanCard />
+          <KanCard />
+        </div>
+      </section>
+      <section>
+        <div className="section-title">
+          <Circle size={13} color="#26b89f" weight="fill" />
           <span>DONE (4)</span>
         </div>
         <div className="cards-wrapper">
+          <KanCard />
+          <KanCard />
+          <KanCard />
           <KanCard />
         </div>
       </section>
