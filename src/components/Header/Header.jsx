cee3dda742ff43babeea353b149325d638bb6e93
diegonacif <diegoreisnacif@gmail.com>
2023-03-04 21:35:58 -0300

    feat: rodal modal with header menu on three dots click

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,51 @@
 import { DotsThreeOutlineVertical } from "phosphor-react"
 import { HeaderMenu } from "../HeaderMenu/HeaderMenu";
-// import { AuthEmailContext } from "../../contexts/AuthEmailProvider";
-import { useContext } from "react";
+import Rodal from 'rodal';
 
 import '../../css/App.css';
+import { useState } from "react";
 
 export const Header = () => {
-  
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  function modalShow() {
+    setIsModalVisible(true);
+  }
+
+  function modalHide() {
+    setIsModalVisible(false);
+  }
+
+  const modalCustomStyles = {
+    height: 'fit-content',
+    width: 'fit-content',
+  }
 
   return (
     <div className="header-container">
       <h3>Nome do quadro</h3>
       <section>
         <button>Adicionar nova tarefa</button>
-        <DotsThreeOutlineVertical size={20} weight="fill" />
+        <DotsThreeOutlineVertical
+          size={20} 
+          weight="fill" 
+          onClick={() => modalShow()}
+        />
       </section>
-      <HeaderMenu />
+      <Rodal
+        visible={isModalVisible}
+        onClose={() => modalHide()}
+        className='rodal-container'
+        id='rodal-dialog'
+        animation='slideRight'
+        duration={400}
+        showMask={true}
+        closeMaskOnClick={true}
+        showCloseButton={false}
+        customStyles={modalCustomStyles}
+      >
+        <HeaderMenu />
+      </Rodal>
     </div>
   )
 }
