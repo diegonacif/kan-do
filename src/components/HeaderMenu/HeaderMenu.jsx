ecb05a7f32b582265568f16cd5addeaef16959d7
diegonacif <diegoreisnacif@gmail.com>
2023-03-05 14:40:42 -0300

    refactor: header menu light mode

--- src/components/HeaderMenu/HeaderMenu.jsx
+++ src/components/HeaderMenu/HeaderMenu.jsx
@@ -1,16 +1,21 @@
 import { useContext } from "react";
 import { AuthEmailContext } from "../../contexts/AuthEmailProvider";
+import { LightModeContext } from "../../contexts/LightModeProvider";
 
 import "../../css/App.css";
 
 export const HeaderMenu = () => {
+  // Email Context
   const { 
     logoutUser,
     isSignedIn,
   } = useContext(AuthEmailContext);
 
+  const { isLightMode } = useContext(LightModeContext); // Light Mode Context
+
+
   return (
-    <div className="header-menu-container">
+    <div className={`header-menu-container ${isLightMode && 'light-mode'}`}>
       <span onClick={() => logoutUser()}>Logout</span>
     </div>
   )
