7c231d9ecb7dfc49caf3ec0ddaec3c75df9430d7
diegonacif <diegoreisnacif@gmail.com>
2023-03-05 16:00:34 -0300

    refactor: kan cards light mode

--- src/components/KanCard/KanCard.jsx
+++ src/components/KanCard/KanCard.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { LightModeContext } from "../../contexts/LightModeProvider";
 import "../../css/App.css";
 
 export const KanCard = () => {
+  const { isLightMode } = useContext(LightModeContext); // Light Mode Context
+
   return (
-    <div className="kan-card-container">
+    <div className={`kan-card-container ${isLightMode && 'light-mode'}`}>
       <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cupiditate tempora sed consequatur modi quod non iure rerum fuga. Cumque repellat reprehenderit nam cupiditate eveniet! Esse reiciendis natus aliquid sed?</span>
     </div>
   )
