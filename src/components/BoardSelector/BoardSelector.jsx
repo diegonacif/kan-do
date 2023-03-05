9102dc1044c0ac46b43210caca1f425822fd27cb
diegonacif <diegoreisnacif@gmail.com>
2023-03-05 14:18:23 -0300

    refactor: board selector light mode

--- src/components/BoardSelector/BoardSelector.jsx
+++ src/components/BoardSelector/BoardSelector.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
+import { LightModeContext } from "../../contexts/LightModeProvider";
 import { Rows } from "phosphor-react"
 import '../../css/App.css';
 
 export const BoardSelector = ({ selected }) => {
+  const { isLightMode } = useContext(LightModeContext); // Light Mode Context
+
   return (
-    <div className={`board-selector-container ${selected && 'selected'}`}>
+    <div className={`board-selector-container ${selected && 'selected'} ${isLightMode && 'light-mode'}`}>
       <Rows size={18} weight="duotone" />
       <span>Platform Launch</span>
     </div>
