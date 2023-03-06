304d707d2e9bb1b6397743f62c4d5fcdb2310a55
diegonacif <diegoreisnacif@gmail.com>
2023-03-06 07:27:49 -0300

    refactor: cards container fixed width

--- src/components/CardsContainer/CardsContainer.jsx
+++ src/components/CardsContainer/CardsContainer.jsx
@@ -36,52 +36,54 @@ export const CardsContainer = ({ refresh }) => {
   
   return (
     <div className={`cards-container-container ${isLightMode && 'light-mode'}`}>
-      <section>
-        <div className="section-title">
-          <Circle size={13} color="#f1e585" weight="fill" />
-          <span>TODO (4)</span>
-        </div>
-        <div className="cards-wrapper">
-          {
-            firestoreLoading ?
-            null :
-            cardsRaw?.map((card) => {
-              return (
-                <KanCard 
-                  key={card.id}
-                  status={card.status}
-                  taskContent={card.taskContent}
-                />
-              )
-            })
-          }
-          {/* <KanCard />
-          <KanCard />
-          <KanCard /> */}
-        </div>
-      </section>
-      <section>
-        <div className="section-title">
-          <Circle size={13} color="#12a9ca" weight="fill" />
-          <span>DOING (4)</span>
-        </div>
-        <div className="cards-wrapper">
-          {/* <KanCard />
-          <KanCard /> */}
-        </div>
-      </section>
-      <section>
-        <div className="section-title">
-          <Circle size={13} color="#26b89f" weight="fill" />
-          <span>DONE (4)</span>
-        </div>
-        <div className="cards-wrapper">
-          {/* <KanCard />
-          <KanCard />
-          <KanCard />
-          <KanCard /> */}
-        </div>
-      </section>
+      <div className="cards-container-wrapper">
+        <section>
+          <div className="section-title">
+            <Circle size={13} color="#f1e585" weight="fill" />
+            <span>TODO (4)</span>
+          </div>
+          <div className="cards-wrapper">
+            {
+              firestoreLoading ?
+              null :
+              cardsRaw?.map((card) => {
+                return (
+                  <KanCard 
+                    key={card.id}
+                    status={card.status}
+                    taskContent={card.taskContent}
+                  />
+                )
+              })
+            }
+            {/* <KanCard />
+            <KanCard />
+            <KanCard /> */}
+          </div>
+        </section>
+        <section>
+          <div className="section-title">
+            <Circle size={13} color="#12a9ca" weight="fill" />
+            <span>DOING (4)</span>
+          </div>
+          <div className="cards-wrapper">
+            {/* <KanCard />
+            <KanCard /> */}
+          </div>
+        </section>
+        <section>
+          <div className="section-title">
+            <Circle size={13} color="#26b89f" weight="fill" />
+            <span>DONE (4)</span>
+          </div>
+          <div className="cards-wrapper">
+            {/* <KanCard />
+            <KanCard />
+            <KanCard />
+            <KanCard /> */}
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
